a7bf9a2e4bef4a988b35abfaf50f988b460de421
Alan Naranjo <85073731+L-A-M-J-O@users.noreply.github.com>
2023-03-22 00:43:52 +0100

    6OA*WId8b@&6

--- PI-Food-main/api/src/models/Recipe.js
+++ PI-Food-main/api/src/models/Recipe.js
@@ -1,12 +1,31 @@
 const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
     name: {
+      type: DataTypes.STRING,
+      allowNull: false, 
+    },
+    imagen: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    resumen: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    nivel: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    paso_a_paso: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   });
 };
